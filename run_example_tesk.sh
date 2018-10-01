9c9900872b25a2e011452ec1781d05cd2cb22832
Foivos Gypas <foivos.gypas@unibas.ch>
2018-10-01 20:57:27 +0200

    Addition of hashsplitter workflow

---
+++ run_example_tesk.sh
@@ -0,0 +1,6 @@
+cwl-tes \
+--leave-outputs \
+--remote-storage-url ftp://ftp-private.ebi.ac.uk/upload/foivos \
+--tes https://tes-dev.tsi.ebi.ac.uk/ \
+hashsplitter-workflow.cwl \
+hashsplitter-test.yml
